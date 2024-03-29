a47a0f5b7e5a5b88f07f9a695007dbe093b7f840
M Aleem <162026993+Aleem12142@users.noreply.github.com>
2024-03-29 10:22:28 -0700

    my first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+let rnadomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "select",
+        type: "list",
+        message: "Select number of dices",
+        choices: ["Single Dice", "Double Dice", "Triple Dice"]
+    },
+]);
+if (answer.select === "Single Dice") {
+    console.log(rnadomNumber);
+}
+else if (answer.select === "Double Dice") {
+    console.log(rnadomNumber);
+    console.log(rnadomNumber);
+}
+else if (answer.select === "Triple Dice") {
+    console.log(rnadomNumber);
+    console.log(rnadomNumber);
+    console.log(rnadomNumber);
+}
+else {
+    console.log("Please select one of given option");
+}
